66c9504f2749e086d7bf61ca05b9d4ffd8618266
Hawk <tsasha1208@rambler.ru>
2021-10-07 21:19:23 +0300

    Rework based on new tests and gui

--- Practice3/Test/test_convert.py
+++ Practice3/Test/test_convert.py
@@ -7,16 +7,16 @@ class ConvertTest(unittest.TestCase):
         self.conv = Convert()
 
     def test_length_conversion(self):
-        self.assertEqual(self.conv.convert_it(1, 'length', 'm', 'cm'), 100)
+        self.assertEqual(self.conv.convert_it(1, self.conv.length, 'm', 'cm'), 100)
 
     def test_square_conversion(self):
-        self.assertEqual(self.conv.convert_it(3, 'square', 'sotka', 'sq. m'), 300)
+        self.assertEqual(self.conv.convert_it(3, self.conv.square, 'sotka', 'sq. m'), 300)
 
     def test_volume_conversion(self):
-        self.assertEqual(self.conv.convert_it(5, 'volume', 'ml', 'c. cm'), 5)
+        self.assertEqual(self.conv.convert_it(5, self.conv.volume, 'ml', 'c. cm'), 5)
 
     def test_mass_conversion(self):
-        self.assertEqual(self.conv.convert_it(16, 'mass', 'centner', 'ton'), 1.6)
+        self.assertEqual(self.conv.convert_it(16, self.conv.mass, 'centner', 'ton'), 1.6)
 
 
 if __name__ == '__main__':
