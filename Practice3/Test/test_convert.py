22e664eac8611107035b84efe0ccf03d83cd148c
Hawk <tsasha1208@rambler.ru>
2021-10-07 16:58:33 +0300

    Added more test for convert_it

--- Practice3/Test/test_convert.py
+++ Practice3/Test/test_convert.py
@@ -6,8 +6,18 @@ class ConvertTest(unittest.TestCase):
     def setUp(self):
         self.conv = Convert()
 
-    def test_conversion(self):
-        self.assertEqual(self.conv.convert_it(1, "Meter", "Centimeter"), 100)
+    def test_length_conversion(self):
+        self.assertEqual(self.conv.convert_it(1, 'length', 'm', 'cm'), 100)
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_square_conversion(self):
+        self.assertEqual(self.conv.convert_it(3, 'square', 'sotka', 'sq. m'), 300)
+
+    def test_volume_conversion(self):
+        self.assertEqual(self.conv.convert_it(5, 'volume', 'ml', 'c. cm'), 5)
+
+    def test_mass_conversion(self):
+        self.assertEqual(self.conv.convert_it(16, 'mass', 'centner', 'ton'), 1.6)
+
+
+if __name__ == '__main__':
+    unittest.main()
